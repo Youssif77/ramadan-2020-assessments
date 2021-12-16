b93cfc35241818c0340bf47decd580df4c5e255b
youssif hany <youssifhany75@gmail.com>
2021-12-16 20:40:00 +0200

    add mongoDB crediatnails

--- server/models/mongo.config.js
+++ server/models/mongo.config.js
@@ -1,19 +1,33 @@
-const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
+const mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
+const dotenv = require("dotenv");
 
-const mongodbUrl = ''; // TODO: PUT YOUR VALID MONGODB CONNECTION URL HERE <-
+dotenv.config({ path: "./.env" });
+
+const mongodbUrl = process.env.DB_URL.replace(
+  "<PASSWORD>",
+  process.env.DB_PASSWORD
+);
 
 if (!mongodbUrl) {
-  console.log('\x1b[33m%s\x1b[0m','Please set the mongodb connection first in -> "server/models/mongo.config.js"\n');
+  console.log(
+    "\x1b[33m%s\x1b[0m",
+    'Please set the mongodb connection first in -> "server/models/mongo.config.js"\n'
+  );
   return;
 }
 
-mongoose.connect(mongodbUrl);
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to Database Video Requests');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to Database Video Requests");
 });
 
 module.exports = mongoose;
--- server/models/video-requests.model.js
+++ server/models/video-requests.model.js
@@ -1,8 +1,8 @@
-var mongoose = require('./mongo.config');
+const mongoose = require("./mongo.config");
 
 if (!Object.keys(mongoose).length) return;
 
-var VideoRequestsSchema = mongoose.Schema(
+const VideoRequestsSchema = mongoose.Schema(
   {
     author_name: String,
     author_email: String,
@@ -13,15 +13,15 @@ var VideoRequestsSchema = mongoose.Schema(
       ups: { type: Number, default: 0 },
       downs: { type: Number, default: 0 },
     },
-    target_level: { type: String, default: 'beginner' },
-    status: { type: String, default: 'new' },
+    target_level: { type: String, default: "beginner" },
+    status: { type: String, default: "new" },
     video_ref: {
-      link: { type: String, default: '' },
-      date: { type: String, default: '' },
+      link: { type: String, default: "" },
+      date: { type: String, default: "" },
     },
   },
-  { timestamps: { createdAt: 'submit_date', updatedAt: 'update_date' } }
+  { timestamps: { createdAt: "submit_date", updatedAt: "update_date" } }
 );
 
-var videoRequestsModel = mongoose.model('VideoRequests', VideoRequestsSchema);
+const videoRequestsModel = mongoose.model("VideoRequests", VideoRequestsSchema);
 module.exports = videoRequestsModel;
