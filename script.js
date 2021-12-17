0beb149a193be0044c1a6116d211e529eb2f7bfa
alimhmd97 <aliiimhmddd99@gmail.com>
2021-12-17 19:17:26 +0200

    dateProblem

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ console.log("holla");
 
 const formInputs = document.querySelectorAll(".form-control");
 const requestBtn = document.querySelector("#request-btn");
+const requestsContainer = document.querySelector("#listOfRequests");
+window.onload = function () {
+  const requests = getRequests();
+  renderRequests(requests);
+};
 
 requestBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -25,6 +30,43 @@ const sendRequest = async (enteredData) => {
 const getRequests = async () => {
   const res = await fetch("http://localhost:7777/video-request");
   const data = await res.json();
-  // console.log(data);
+  console.log(data);
+  return data;
 };
-getRequests();
+const renderRequests = (data) => {
+  data.forEach((item) => {
+    `      <div class='card mb-3'>
+        <div class='card-body d-flex justify-content-between flex-row'>
+          <div class='d-flex flex-column'>
+            <h3>${item.topic_title}</h3>
+            <p class='text-muted mb-2'>${item.topic_details}</p>
+            <p class='mb-0 text-muted'>
+              <strong>${item.expected_result}:</strong>
+              Dummy expected result text
+            </p>
+          </div>
+          <div class='d-flex flex-column text-center'>
+            <a class='btn btn-link'>🔺</a>
+            <h3>0</h3>
+            <a class='btn btn-link'>🔻</a>
+          </div>
+        </div>
+        <div class='card-footer d-flex flex-row justify-content-between'>
+          <div>
+            <span class='text-info'>NEW</span>
+            &bullet; added by
+            <strong>${item.author_name}</strong>
+            on
+            <strong>Sat Apr 11 2020</strong>
+          </div>
+          <div class='d-flex justify-content-center flex-column 408ml-auto mr-2'>
+            <div class='badge badge-success'>Beginners</div>
+          </div>
+        </div>
+      </div>`;
+  });
+};
+const transformDate = (date) => {
+  const modeffiedDate = new Date(date);
+  return `${modeffiedDate} `;
+};
