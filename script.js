0b5b02bfb2f9193f27788058200ce40ea4572021
youssif hany <youssifhany75@gmail.com>
2021-12-17 16:10:00 +0200

    add btn-listener

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+"use strict";
+console.log("holla");
 
+const formInputs = document.querySelectorAll(".form-control");
+const requestBtn = document.querySelector("#request-btn");
+
+requestBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const inputsObj = {};
+  formInputs.forEach((formInput) => {
+    inputsObj[formInput.name] = formInput.value;
+  });
+});
+
+const sendRequest = async (data) => {
+  const res = await fetch("");
+};
